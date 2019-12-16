9016ee597ed4d07fe776e9bacb9cb4a5ea5f108b
Frank Runfola <Frunfola1229@gmail.com>
2019-12-16 16:25:48 -0500

    Add code for table properties

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
-const table = document.createElement('table');
+const btn = document.querySelector("button")
 
-table.style.border = "5px solid red"
+const fetchPlanets = (data) => {
+    fetch("https://swapi.co/api/planets/") //
+        .then((response) => response.json())
+        .then(getPlanets)
+        .then()
+        .catch(error => {
+            console.log(error)
+        })
 
-console.log("hello")
-fetch("https://swapi.co/api/planets/") //
-    .then((response) => response.json())
-    .then(data => {
-        for (let planet of data.results) {
+    btn.innerText = "....loading"
 
-            console.log(planet)
-            const row = document.createElement('tr');
-            row.style.border = "2px solid red"
-            row.align = "center"
-
-            const tableElem1 = document.createElement('td');
-            tableElem1.style.border = "2px solid red"
-            tableElem1.style.width = "100px"
-            tableElem1.style.height = "100px"
-            tableElem1.innerText = planet.name
-            row.appendChild(tableElem1)
-
-            const tableElem2 = document.createElement('td');
-            tableElem2.style.border = "2px solid red"
-            tableElem2.style.width = "100px"
-            tableElem2.style.height = "100px"
-            tableElem2.innerText = planet.terrain
-            row.appendChild(tableElem2)
-
-            const tableElem3 = document.createElement('td');
-            tableElem3.style.border = "2px solid red"
-            tableElem3.style.width = "100px"
-            tableElem3.style.height = "100px"
-            tableElem3.innerText = planet.population
-            row.appendChild(tableElem3)
-            table.appendChild(row)
-
-
-            console.log(planet.name)
-        }
-    })
-
+}
+btn.addEventListener("click", fetchPlanets)
 
 const cont = document.querySelector(".container")
+
+const table = document.createElement('table');
+const rowHeader = document.createElement('tr')
+
+const dataHeader1 = document.createElement('th');
+const dataHeader2 = document.createElement('th');
+const dataHeader3 = document.createElement('th');
+
+dataHeader1.innerText = "PlanetName"
+dataHeader2.innerText = "Population"
+dataHeader3.innerText = "PlanetName"
+
+rowHeader.appendChild(dataHeader1)
+rowHeader.appendChild(dataHeader2)
+rowHeader.appendChild(dataHeader3)
+rowHeader.align = "center"
+table.appendChild(rowHeader)
+
+
+let count = 0
+
+const getPlanets = (data) => {
+    for (let planet of data.results) {
+        console.log(planet)
+        const row = document.createElement('tr');
+        row.align = "center"
+
+        const tableElem0 = document.createElement('td');
+        tableElem0.innerText = `${++count}`
+
+        row.appendChild(tableElem0)
+
+        const tableElem1 = document.createElement('td');
+        tableElem1.innerText = planet.name
+        row.appendChild(tableElem1)
+
+        const tableElem3 = document.createElement('td');
+        tableElem3.innerText = planet.population
+        row.appendChild(tableElem3)
+
+        table.appendChild(row)
+        console.log(planet.name)
+
+        btn.innerText = "FINISHED!!"
+    }
+}
+
+
 cont.align = "center"
 cont.appendChild(table)
