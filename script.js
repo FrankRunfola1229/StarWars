a8be225a5370403e0ae3f9060ce373f1792418fb
Runfola,Frank <runfola.frank@healthnow.org>
2019-12-17 13:59:09 -0500

    added modular refacting and better-looking table gui

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-/**************************************************************
-
- **************************************************************/
 const fetchPlanets = (data) => {
 	fetch("https://swapi.co/api/planets/") //
 		.then((response) => response.json())
@@ -11,18 +8,14 @@ const fetchPlanets = (data) => {
 	btn.innerText = "....loading";
 };
 
-/**************************************************************
-
- **************************************************************/
 const getPlanets = (data) => {
+	let count = 0;
 	for (let planet of data.results) {
 		createTableRow(++count, planet.name, planet.population);
 	}
 	btn.innerText = "FINISHED!!";
 };
-/**************************************************************
 
- **************************************************************/
 const createTableRow = (count, name, population) => {
 	const tr = document.createElement("tr");
 	createTableData(count, tr);
@@ -30,19 +23,15 @@ const createTableRow = (count, name, population) => {
 	createTableData(population, tr);
 	table.appendChild(tr);
 };
-/**************************************************************
 
- **************************************************************/
 const createTableData = (tdDate, tr) => {
 	const td = document.createElement("td");
 	td.innerText = tdDate;
 	tr.appendChild(td);
 };
 
-let count = 0;
 const btn = document.querySelector("button");
 btn.addEventListener("click", fetchPlanets);
-
 const cont = document.querySelector(".container");
 const table = document.querySelector("table");
 const thead = document.querySelector("table thead");
