104dfe1fd871fd00c746275bf29c12e2910d6f11
Runfola,Frank <runfola.frank@healthnow.org>
2019-12-18 12:44:00 -0500

    added total div under button

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
+const gears = document.querySelector("#gearsContainer");
 const btn = document.querySelector("button");
+const total = document.querySelector("#total");
 const cont = document.querySelector(".container");
 const tableBody = document.querySelector("table tbody");
 const thead = document.querySelector("table thead");
 const thCount = document.querySelector("#count");
 const thPlanetName = document.querySelector("#planetName");
 const thPopulation = document.querySelector("#population");
+let count = 0;
 
 const fetchPlanets = (data) => {
 	fetch("https://swapi.co/api/planets/") //
@@ -17,28 +20,33 @@ const fetchPlanets = (data) => {
 };
 
 const getPlanets = (data) => {
-	let count = 0;
 	for (let planet of data.results) {
 		createTableRow(++count, planet.name, planet.population);
 	}
 	btn.innerText = "FINISHED!!";
 	gears.style.display = "none";
+	total.innerText = `Total=${count}`;
 };
 
 const createTableRow = (count, name, population) => {
 	const tr = document.createElement("tr");
-	tr.setAttribute("scope", "row");
-	createTableData(count, tr);
-	createTableData(name, tr);
-	createTableData(population, tr);
+	createTh(count, tr);
+	createTd(name, tr);
+	createTd(population, tr);
 	tableBody.appendChild(tr);
 };
 
-const createTableData = (tdDate, tr) => {
+const createTh = (data, tr) => {
+	const th = document.createElement("th");
+	th.innerText = data;
+	th.setAttribute("scope", "row");
+	tr.appendChild(th);
+};
+
+const createTd = (data, tr) => {
 	const td = document.createElement("td");
-	td.innerText = tdDate;
+	td.innerText = data;
 	tr.appendChild(td);
 };
 
 btn.addEventListener("click", fetchPlanets);
-const gears = document.querySelector("#gearsContainer");
